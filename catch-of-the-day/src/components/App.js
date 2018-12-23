4e47e13c32ec06272c2c73a145746e25e4174e2c
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-23 12:43:16 -0800

    Completed video 13, learned about state, added to App, log state to console on submitp

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,25 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends Component {
+  // pass data down from higher Parent Component to share with other Components, using state. can set state with constructor or a property
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    //1. take a copy of existing state. never want to modify state directly (called mutation). not necessary to do deep clone
+    const fishes = { ...this.state.fishes };
+    // 2. Add new fish to the fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    //3. Set the new fishes object to state
+    // take copy of old state and merges it with new state
+    // this.setState({
+    // fishes: fishes
+    // });
+    // if property and value are the same, can just have one of them
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -15,7 +34,7 @@ class App extends Component {
           <Header tagline="I am cool" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
