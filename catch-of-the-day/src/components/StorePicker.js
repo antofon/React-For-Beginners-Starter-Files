6f48c6a76b5a02c71caf91716c6ef845b712a5ab
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-23 10:56:37 -0800

    Video 10 complete, imported helper function code

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
   render() {
     return (
       <form action="" className="store-selector">
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        {/* in React, if you put a value on an <input> you need to attach it to state. with our helpers file, if you want default text, you can use the defaultValue property. When the component mounts to the page, the getFunName function runs */}
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store -></button>
         {/* comment */}
       </form>
