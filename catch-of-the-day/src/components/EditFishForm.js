ab4472c7efda05ea1e28d896c9f5a4260ede2d66
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-24 12:09:47 -0800

    Completed video 20 about bi-directional data flow and live state editing

---
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import AddFishForm from "./AddFishForm";
+
+class EditFishForm extends Component {
+  handleChange = event => {
+    //similar to using refs, another way to grab event and the value out of it
+    console.log(event.currentTarget.value);
+    //update that fish
+    //1. Take a copy of the current fish
+    const updatedFish = {
+      ...this.props.fish,
+      //want the value that's being updated to also be dynamic, using []
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    //send changes up to App component because that's where state lives
+    this.props.updateFish(this.props.index, updatedFish);
+  };
+  render() {
+    return (
+      <div className="fish-edit">
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select
+          type="text"
+          name="status"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea
+          name="desc"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
+      </div>
+    );
+  }
+}
+
+export default EditFishForm;
